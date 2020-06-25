e84d78ea44c7e77c4422824ff99653db8d65a2e6
Wenyu Ding <wd87@cornell.edu>
2020-06-25 16:44:04 -0400

    changed para names into flow_file and topof, added test files

--- scratch/third.cc
+++ scratch/third.cc
@@ -378,14 +378,14 @@ int main(int argc, char *argv[])
 				else
 					std::cout << "L2_BACK_TO_ZERO\t\t\t" << "No" << "\n";
 			}
-			else if (key.compare("TOPOLOGY_FILE") == 0)
+			else if (key.compare("topof") == 0)
 			{
 				std::string v;
 				conf >> v;
 				topology_file = v;
 				std::cout << "TOPOLOGY_FILE\t\t\t" << topology_file << "\n";
 			}
-			else if (key.compare("FLOW_FILE") == 0)
+			else if (key.compare("flow_file") == 0)
 			{
 				std::string v;
 				conf >> v;
